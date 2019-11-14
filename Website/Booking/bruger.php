1ce6a804a4516057ecbcbac17bf734cee2c9057c
PB0602 <pb28062002@gmail.com>
2019-11-14 12:59:54 +0100

    bruger

---
+++ Website/Booking/bruger.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+<link rel="stylesheet" type="text/css" href="dropdownStyle.css">
+
+	</head>
+	<body>
+
+		<nav>
+			<p>Computarum Booking</p>
+
+		</nav>
+
+		<div id="nav">
+
+			<ul>
+				<li><a href ="#">Lokaler</a>
+							<ul>
+								<li><a href = "#">Lokale 1</a></li>
+								<li><a href = "#">Lokale 2</a></li>
+								<li><a href = "#">Lokale 3</a></li>
+								<li><a href = "#">Lokale 4</a></li>
+							</ul>
+				</li>
+					<li><a href ="#">3D-printere</a>
+					<li><a href ="#">Græsslåmaskiner</a>
+					<li><a href ="#">Hatte</a>
+			</ul>
+
+
+		 </div>
+
+	</body>
+</html>
