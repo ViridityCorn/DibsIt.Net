e2948ab8d468caaf8f01b9f3d6265d8378ff8de1
KlokkeBlomst <lajsen1@gmail.com>
2019-11-14 12:59:42 +0100

    Gl

--- Website/Booking/index.php
+++ Website/Booking/index.php
@@ -4,8 +4,10 @@
 </head>
 <body>
 <?php
-include 'calendar.php';
- 
+//Include lader en ligge andre dokumenter in i index så det arbejder sammen.
+include ('1-reserve.sql');
+include ('2c-ajax-reserve.php');
+include ('calendar.php');
 $calendar = new Calendar();
  
 echo $calendar->show();
