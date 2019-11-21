f4a850fda6c3b9c95f32c516ee7736deae7ceb25
PB0602 <pb28062002@gmail.com>
2019-11-21 12:18:51 +0100

    Create bruger.php

---
+++ DibsIt/Booking/bruger.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+<link rel="stylesheet" type="text/css" href="brugerStyle.css">
+
+	</head>
+	<body>
+
+		<nav>
+			<p>Computarum Booking</p>
+
+			<button type="button" >Log ind</button>
+
+		</nav>
+
+
+
+		<div id="nav">
+			<p>Materialer</p>
+
+			<ul>
+				<li><a href ="#">Lokaler</a>
+							<ul>
+								<li><a href = "#">Lokale 1</a></li>
+								<li><a href = "#">Lokale 2</a></li>
+								<li><a href = "#">Lokale 3</a></li>
+								<li><a href = "#">Lokale 4</a></li>
+							</ul>
+				</li>
+					<li><a href ="#">3D-printere</a>
+					<li><a href ="#">Græsslåmaskiner</a>
+					<li><a href ="#">Hatte</a>
+			</ul>
+
+
+		 </div>
+
+		 <div class = "search-bar">
+			 <input class="search-input" type = "text" name=""placeholder="Søg">
+			 <a class = "search-btn" href = "#">
+
+			 </a>
+		 </div>
+
+	</body>
+</html>
