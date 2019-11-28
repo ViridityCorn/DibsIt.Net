8d353e495ee8221654e52cfd82383a265b4a2d36
ViridityCorn <davidfoght@gmail.com>
2019-11-28 11:24:43 +0100

    Added brugerside

--- DibsIt/users/template/brugerside.php
+++ DibsIt/users/template/brugerside.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <style>
+.center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+    </style>
 </head>
-<body>
-    
-</body>
+  <body>
+      <section class="hero is-fullheight has-background-black">
+          <div class="hero-body">
+              <div class="container">
+                  <figure class="image center">
+                      <img src="../../system/assets/LogoWHITE.png" style="max-width: 512px;">
+                  </figure>
+              </div>
+          </div>
+      </section>
+  </body>
 </html>
